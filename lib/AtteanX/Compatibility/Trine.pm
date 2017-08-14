f17ca44d6198e0958bf8d7bc80bcca8ae3925f4a
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-08-14 13:26:58 +0200

    Initial commit

---
+++ lib/AtteanX/Compatibility/Trine.pm
@@ -0,0 +1,55 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package Attean::IRI {
+	sub uri {
+		my $self	= shift;
+		return $self->abs;
+	}
+}
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+AtteanX::Compatibility::Trine - Compatibility layer between RDF::Trine and Attean
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=AtteanX-Compatibility-Trine>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2017 by Kjetil Kjernsmo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
